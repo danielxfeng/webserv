5f97dee33c7e9813ddd6c8edc7a9c644daa0a066
Gregory Pellechi <greg.pellechi@gmail.com>
2025-08-06 15:31:24 +0300

    Added a concept to ensure Args variables are streamable

--- includes/LogSys.hpp
+++ includes/LogSys.hpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <iomanip>
 #include <chrono>
+#include <type_traits>
+#include <concepts>
 
 typedef enum	e_priority
 {
@@ -14,6 +16,12 @@ typedef enum	e_priority
 	ERROR,
 	FATAL,
 }	t_priority;
+
+// Checks if Args are types that can be streamed to std::cout
+template<typename T>
+concept Streamable = requires(std::ostream& os, T t) {
+    { os << t } -> std::same_as<std::ostream&>;
+};
 
 class LogSys{
 private:
@@ -30,14 +38,15 @@ private:
 		return (logger);
 	}
 
-	template<typename... Args>
-	void	log(const char* msg_priority_str, t_priority message_priority,const char* function, const char* message, Args&&... args)
+	template<Streamable... Args>
+	void	log(const char* msg_priority_str, t_priority message_priority,
+	  const char* function, const char* message, Args&&... args)
 	{
 		if (priority_ <= message_priority)
 		{
 			auto now = std::chrono::system_clock::now();
 			auto now_time_t = std::chrono::system_clock::to_time_t(now);
-			auto now_ms = duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
+			auto now_ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
 			std::tm local_tm = *std::localtime(&now_time_t);
 			std::cout << std::put_time(&local_tm, "%H:%M:%S");
 			std::cout << '.' << std::setfill('0') << std::setw(3) << now_ms.count();
