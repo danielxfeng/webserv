0ff906b515f2dff7053589c15910ef3cfddd9ca5
Gregory Pellechi <greg.pellechi@gmail.com>
2025-08-20 09:42:04 +0300

    Some work, waiting on new data structure

--- includes/MethodHandler.hpp
+++ includes/MethodHandler.hpp
@@ -2,7 +2,6 @@
 
 #include "LogSys.hpp"
 #include "WebServErr.hpp"
-#include "utils.hpp"
 
 #include <unordered_map>
 #include <vector>
@@ -13,8 +12,18 @@
 #include <cstdio>
 #include <unistd.h>
 #include "SharedEnums.hpp"
+#include <chrono>
+#include <ctime>
+
+#define MAX_BODY_SIZE	1024
 
 
+typedef struct	s_FormData
+{
+	std::string	name_;
+	std::string	type_;
+	std::string	content_;
+}	t_FormData;
 
 class MethodHandler
 {
@@ -30,6 +39,11 @@ private:
 	void	setContentLength(std::unordered_map<std::string, std::string> headers);
 	void	checkContentLength(std::unordered_map<std::string, std::string> headers) const;
 	void	checkContentType(std::unordered_map<std::string, std::string> headers) const;
+	void	parseBoundaries(const std::string &boundary, std::vector<t_FormData>& sections);
+	void	checkIfRegFile(const std::string &path);
+	void	checkIfDirectory(const std::string &path);
+	void	checkIfLocExists(const std::string &path);
+	std::string	createFileName(const std::string &path);
 public:
     MethodHandler();
     MethodHandler(const MethodHandler &copy);
--- srcs/MethodHandler.cpp
+++ srcs/MethodHandler.cpp
@@ -74,6 +74,7 @@ int		MethodHandler::callGetMethod(std::string &path)
 		return (fd_);
 	}
 	checkIfRegFile(path);
+	//TODO Check if is index or auto index
 	if (!access(path, R_OK))
 	{
 		LOG_INFO("Permission Denied, cannot access: ", path);
@@ -92,22 +93,37 @@ int		MethodHandler::callPostMethod(std::string &path, std::unordered_map<std::st
 {
 	checkIfLocExists(path);
 	checkIfDirectory(path);
-	auto typeCheck = headers.find("Type");//TODO Check with Mohammad what this will be
+	auto typeCheck = headers.find("content-type");
 	setContentLength(headers);
 	if (typeCheck == headers.end())
 	{
 		LOG_ERROR("Bad Request, Type not found.", typeCheck);
 		throw WebServErr::MethodException(ERR_400, "Bad Request, Type not found.");
 	}
-	if (typeCheck->second.find("multipart/form"))
+	if (typeCheck->second.find("multipart/form-data") == 0)
 	{
 		//TODO
+		std::string type = headers.at("content-type");
+		std::string start = "multipart/form-data; boundary=";
+		if (type.find(start) == std::string::npos)
+		{
+			LOG_ERROR("Found no boundary data: ", type);
+			WebServErr::MethodException(ERR_400, "Found no boundary data.");
+		}
+		std::string boundary = type.substr(start.size());
+		//TODO Construct form data - check with Mohammad
+	}
+	else
+	{
+
 	}
 	checkContentType(headers);
-	fd_ = open(path.c_str(), O_WRONLY | O_CREAT | O_APPEND | O_NONBLOCK, 0644);
+	//TODO get file name from headers
+	std::string filename = createFileName(path);
+	fd_ = open(filename.c_str(), O_WRONLY | O_CREAT | O_APPEND | O_NONBLOCK, 0644);
 	if (fd_ == -1)
 	{
-		LOG_FATAL("Failed to create: ", path);
+		LOG_FATAL("Failed to create: ", filename);
 		throw WebServErr::SysCallErrException("Failed to create file");
 	}
 	return (fd_);
@@ -139,10 +155,10 @@ int		MethodHandler::callCGIMethod(std::string &path)
 
 void	MethodHandler::setContentLength(std::unordered_map<std::string, std::string> headers)
 {
-	auto check = headers.find("Length");
+	auto check = headers.find("content-length");
 	if (check == headers.end())
 	{
-		LOG_ERROR("Content Length Required.", (void);
+		LOG_ERROR("Content Length Required.", (void));
 		throw WebServErr::MethodException(ERR_400, "Failed to get content length.");//TODO double check error code
 	}
 	std::stringstream length(check->second);
@@ -157,7 +173,7 @@ void	MethodHandler::setContentLength(std::unordered_map<std::string, std::string
 		LOG_ERROR("Failed to get the end of file for content length ", length);
 		throw WebServErr::MethodException(ERR_400, "Bad Request, failed to get end of file for content length.");//TODO double check error code
 	}
-	if (expectedLength_ > MAX_BODY_SIZE)//TODO set macro for max body size
+	if (expectedLength_ > MAX_BODY_SIZE)
 	{
 		LOG_ERROR("Content length greater than max body size ", expectedLength_);
 		throw WebServErr::MethodException(/*TODO*/, "Body size too large.");
@@ -166,24 +182,71 @@ void	MethodHandler::setContentLength(std::unordered_map<std::string, std::string
 
 void	MethodHandler::checkContentLength(std::unordered_map<std::string, std::string> headers) const
 {
-	auto check = headers.find("Length");
-	if (/*TODO length check here*/)
+	auto check = headers.find("content-length");
+	if (/*TODO length check here*/ != expectedLength_ || /*TODO*/ > MAX_BODY_SIZE)
 	{
-		LOG_ERROR("Bad Request, Body Length does not equal Expected Length ", check->second());
+		LOG_ERROR("Bad Request, Body Length does not equal Expected Length ", check->second);
 		throw WebServErr::MethodException(ERR_400, "Bad Request, Body Length does not equal Expected Length.");
 	}
 }
 
-void	MethodHanlder::checkContentType(std::unordered_map<std::string, std::string> headers) const
+void	MethodHandler::checkContentType(std::unordered_map<std::string, std::string> headers) const
 {
-	auto typeCheck = headers.find("Type");//TODO Check with Mohammad what this will be
-	if (typeCheck->second.find("multipart/form"))
+	auto typeCheck = headers.find("content-type");
+	if (typeCheck->second.find("multipart/form-data"))
 	{
 		//TODO loop through parts and check MIMETYPE, if not throw bad request
 	}
 	else if (/*TODO chceck MIMEType of path against content type*/)
 	{
-		LOG_ERROR("Bad Request, Content Type does not match ", typecheck->second());
+		LOG_ERROR("Bad Request, Content Type does not match ", typeCheck->second);
 		throw WebServErr::MethodException(ERR_400, "Bad Request, Content Type does not match.");
 	}
 }
+
+void	MethodHandler::parseBoundaries(const std::string &boundary, std::vector<t_FormData>& sections)
+{
+
+}
+
+void    MethodHandler::checkIfRegFile(const std::string &path)
+{
+    if (!std::filesystem::is_regular_file(path))
+	{
+		LOG_ERROR("File is not a regular file: ", path);
+		throw WebServErr::MethodException(ERR_500, "File is not a regular file");
+	}
+}
+
+void    MethodHandler::checkIfDirectory(const std::string &path)
+{
+    if (std::filesystem::is_directory(path))
+	{
+		LOG_WARN("Target is a Directory: ", path);
+		throw WebServErr::MethodException(ERR_403, "Target is a directory");
+	}
+}
+
+void    MethodHandler::checkIfLocExists(const std::string &path)
+{
+    if (!std::filesystem::exists(path))
+	{
+		LOG_ERROR("Permission Denied: Cannot Delete: ", path);
+		throw WebServErr::MethodException(ERR_500, "Permission Denied, cannot delete selected file");
+	}
+}
+
+std::string	MethodHandler::createFileName(const std::string &path)
+{
+	//TODO trim .png
+	std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+	time_t tt = std::chrono::system_clock::to_time_t(now);
+	tm	local_tm = *localtime(&tt);
+	std::string year = std::to_string(local_tm.tm_year + 1900);
+	std::string month = std::to_string(local_tm.tm_mon + 1);
+	std::string day = std::to_string(local_tm.tm_mday);
+	std::string filename = /*TODO directory*/ path + '_' + year + '_' + month + '_' + day + '.png';
+	checkIfLocExists(filename);
+	return (filename);
+}
+
