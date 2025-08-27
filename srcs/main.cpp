d1d7d1422ed2acc3e95824519a86ea7f8aaf6fbf
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-08-27 11:11:03 +0300

    refactor: improve config file validation and add default configuration

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,39 +1,45 @@
 #include "WebServ.hpp"
 
-
-
-bool validateConfigFile(std::string_view &fileName){
+bool validateConfigFile(std::string_view &fileName)
+{
 	size_t found;
 
 	found = fileName.find(".json");
-	if(found == std::string::npos){
-		std::cerr<<"Error, you have to include the config file.";
+	if (found == std::string::npos)
+	{
+		std::cerr << "Error, you have to include the config file.";
 		return (false);
 	}
-	if (found != (fileName.size() - 5)){
-		std::cerr<<"Error, file name is not correct.";
+	if (found != (fileName.size() - 5))
+	{
+		std::cerr << "Error, file name is not correct.";
 		return (false);
 	}
 
 	return (true);
 }
 
-
 int main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		std::cerr<<"Error, you have to include the config file.";
+		std::cerr << "Error, you have to include the config file.";
 		return (1);
 	}
-	std::string_view fileName= argv[1];
-	if(!validateConfigFile(fileName)){
+
+	try
+	{
+		std::string_view fileName = argv[1];
+		if (!validateConfigFile(fileName))
+			throw std::invalid_argument("Invalid configuration file.");
+		auto webserv = WebServ(argv[1]);
+		webserv.eventLoop();
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
 		return (1);
-		
 	}
 
-
-	auto webserv = WebServ(argv[1]);
-	webserv.eventLoop();
-	return (0);	
+	return (0);
 }
