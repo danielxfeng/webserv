4f170b072a43e280798202463b3da6c0ce6f1e2d
Gregory Pellechi <greg.pellechi@gmail.com>
2025-09-25 13:28:59 +0300

    Can now create random file name

--- includes/MethodHandler.hpp
+++ includes/MethodHandler.hpp
@@ -15,6 +15,10 @@
 #include <algorithm>
 #include <fstream>
 #include <unistd.h>
+#include <iomanip>
+#include <ctime>
+#include <random>
+#include <unistd.h>
 
 #include "SharedTypes.hpp"
 #include "LogSys.hpp"
@@ -51,9 +55,8 @@ private:
 
 	std::string matchLocation(std::unordered_map<std::string, t_location_config> &locations, std::string &targetRef);
 	std::string	stripLocation(const std::string &server, const std::string &target);
-	std::filesystem::path createFileName(const std::string &path);
 	std::filesystem::path createRealPath(const std::string &server, const std::string &target);
-	std::filesystem::path createPostFilename(std::filesystem::path &path, std::unordered_map<std::string, std::string> requestBody);
+	std::filesystem::path createRandomFilename(std::filesystem::path &path, std::string &extension);
 	std::string generateDynamicPage(std::filesystem::path &path);
 	bool	canAccess(std::filesystem::path &path, t_access access_type);
 public:
