2cc666bed6226f6b3f2973c349e03a7c98909d21
Gregory Pellechi <greg.pellechi@gmail.com>
2025-09-28 17:05:17 +0300

    Added errPages to server_config

--- includes/ErrorReponse.hpp
+++ includes/ErrorReponse.hpp
@@ -14,8 +14,6 @@ class ErrorResponse
 private: 
     t_file  response_;
 
-    std::string errorCodeToString(t_status_error_codes &code);
-    std::filesystem::path getFilename(std::string &root, std::string &codeStr);
 public:
     ErrorResponse() = delete;
     ErrorResponse(EpollHelper &epoll_helper);
@@ -23,5 +21,5 @@ public:
     ErrorResponse &operator= (const ErrorResponse &copy) = delete;
     ~ErrorResponse();
 
-    t_file  getErrorPage(std::unordered_map<std::string, t_location_config> &locations, t_status_error_codes &code);
+    t_file  getErrorPage(std::unordered_map<t_status_error_codes, std::string> &errPages, t_status_error_codes &code);
 };
--- srcs/ErrorResponse.cpp
+++ srcs/ErrorResponse.cpp
@@ -15,45 +15,18 @@ ErrorResponse::~ErrorResponse()
     LOG_TRACE("Error Response ", "Deconstructed");
 }
 
-t_file  ErrorResponse::getErrorPage(std::unordered_map<std::string, t_location_config> &locations, t_status_error_codes &code)
+t_file  ErrorResponse::getErrorPage(std::unordered_map<t_status_error_codes, std::string> &errPages, t_status_error_codes &code)
 {
     LOG_TRACE("Getting Error Reponse page for code: ", code);
-    std::string codeStr = errorCodeToString(code);
-    std::string intenedRoot = "error/";
-    //TODO get error root directory, if that fails get default error directory
-    std::filesystem::path errorFilename = ;//TODO get file
+    if (!errPages.contains(code))
+        throw WebServErr::ErrorResponseException("Error Page does not exist");
+    std::filesystem::path errorFilename = errPages.contains(code);
     if (access(errorFilename.c_str(), R_OK) == -1)
-        throw WebServErr::ErrorResponseException::ErrorResponseException("Cannot access the error response page");
+        throw WebServErr::ErrorResponseException("Cannot access the error response page");
     response_.FD_handler_OUT->setFd(open(errorFilename.c_str(), O_RDONLY | O_NONBLOCK));
     if (response_.FD_handler_OUT.get()->get() == -1)
-        throw WebServErr::ErrorResponseException::ErrorResponseException("Failed to open the error reponse page");
+        throw WebServErr::ErrorResponseException("Failed to open the error reponse page");
     response_.fileSize = static_cast<int>(std::filesystem::file_size(errorFilename));
     LOG_TRACE("Returning Error Response via FD: ", response_.FD_handler_OUT);
     return (std::move(response_));
-}
-
-std::string ErrorResponse::errorCodeToString(t_status_error_codes &code){
-    switch (code)
-    {
-        case (ERR_NO_ERROR)
-            return ("");
-        case (ERR_301_REDIRECT)
-            return ("301");
-        case (ERR_400_BAD_REQUEST)
-            return ("400");
-        case (ERR_401_UNAUTHORIZED)
-            return ("401");
-        case (ERR_403_FORBIDDEN)
-            return ("403");
-        case (ERR_404_NOT_FOUND)
-            return ("404");
-        case (ERR_405_METHOD_NOT_ALLOWED)
-            return ("405");
-        case (ERR_409_CONFLICT)
-            return ("409");
-        case (ERR_500_INTERNAL_SERVER_ERROR)
-            return ("500");
-        default:
-            return ("");
-    }
 }
