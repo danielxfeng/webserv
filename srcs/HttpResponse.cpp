9862a34186a5b532c1b1a2b603eae28e23e1add2
mohdkhlouf <mohammad.khlouf@gmail.com>
2025-10-01 14:43:40 +0300

    Fix content lenght after cookies

--- srcs/HttpResponse.cpp
+++ srcs/HttpResponse.cpp
@@ -40,7 +40,7 @@ std::string HttpResponse::successResponse(t_conn *conn, Cookie &cookie)
     {
         result.append("HTTP/1.1").append(" 200 OK\r\n");
         result.append("Content-Type: ").append(content_type).append("\r\n");
-        result.append(cookieStr).append("\r\n");
+        result.append(cookieStr);
         result.append("Content-Length: ").append(std::to_string(conn->res.fileSize)).append("\r\n\r\n");
         if (conn->res.isDynamic)
             result.append(conn->res.dynamicPage);
@@ -50,7 +50,7 @@ std::string HttpResponse::successResponse(t_conn *conn, Cookie &cookie)
         result.append("HTTP/1.1").append(" 201 Created\r\n");
         result.append("Content-Type: ").append(content_type).append("\r\n");
         result.append("Location: ").append(conn->res.postFilename).append("\r\n");
-        result.append(cookieStr).append("\r\n");
+        result.append(cookieStr);
         result.append("Content-Length: 0").append("\r\n\r\n");
     }
     else if (request->getHttpRequestMethod() == "DELETE")
@@ -65,7 +65,7 @@ std::string HttpResponse::successResponse(t_conn *conn, Cookie &cookie)
                                     "</html>";
         result.append("HTTP/1.1").append(" 204 OK\r\n");
         result.append("Content-Type: text/html\r\n");
-        result.append(cookieStr).append("\r\n");
+        result.append(cookieStr);
         result.append("Content-Length: ").append(std::to_string(deleteSuccess.size())).append("\r\n\r\n");
         result.append(deleteSuccess);
     }
@@ -123,12 +123,11 @@ std::string HttpResponse::failedResponse(t_conn *conn, t_status_error_codes erro
                         "<html>"
                         "<head><title>");
         htmlPage.append(status).append("</title></head><body><h1>");
-
         htmlPage.append(status).append("</h1><p>").append(error_message);
         htmlPage.append("</p></body></html>");
         result.append("HTTP/1.1").append(" ").append(status).append("\r\n");
         result.append("Content-Type: text/html\r\n");
-        result.append(cookieStr).append("\r\n");
+        result.append(cookieStr);
         result.append("Content-Length: ").append(std::to_string(htmlPage.size())).append("\r\n\r\n");
         result.append(htmlPage);
     }
@@ -136,7 +135,7 @@ std::string HttpResponse::failedResponse(t_conn *conn, t_status_error_codes erro
     {
         result.append("HTTP/1.1").append(" ").append(status).append("\r\n");
         result.append("Content-Type: text/html\r\n");
-        result.append(cookieStr).append("\r\n");
+        result.append(cookieStr);
         result.append("Content-Length: ").append(std::to_string(errPageSize)).append("\r\n\r\n");
     }
     return (result);
