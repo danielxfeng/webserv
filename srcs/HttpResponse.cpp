8a97e459803f727ba61105a4ba85ddf172280e52
mohdkhlouf <mohammad.khlouf@gmail.com>
2025-10-01 13:46:27 +0300

    adding to post and delete files

--- srcs/HttpResponse.cpp
+++ srcs/HttpResponse.cpp
@@ -49,7 +49,7 @@ std::string HttpResponse::successResponse(t_conn *conn, Cookie &cookie)
         result.append("HTTP/1.1").append(" 201 Created\r\n");
         result.append("Content-Type: ").append(content_type).append("\r\n");
         result.append("Location: ").append(conn->res.postFilename).append("\r\n");
-        result.append("Content-Length: 0").append("\r\n\r\n");
+        result.append("Content-Length: 0").append("Set-Cookie: ").append(cookieStr).append("\r\n\r\n");
     }
     else if (request->getHttpRequestMethod() == "DELETE")
     {
@@ -63,7 +63,7 @@ std::string HttpResponse::successResponse(t_conn *conn, Cookie &cookie)
                                     "</html>";
         result.append("HTTP/1.1").append(" 204 OK\r\n");
         result.append("Content-Type: text/html\r\n");
-        result.append("Content-Length: ").append(std::to_string(deleteSuccess.size())).append("\r\n\r\n");
+        result.append("Content-Length: ").append(std::to_string(deleteSuccess.size())).append("Set-Cookie: ").append(cookieStr).append("\r\n\r\n");
         result.append(deleteSuccess);
     }
     std::cout << result << std::endl;
